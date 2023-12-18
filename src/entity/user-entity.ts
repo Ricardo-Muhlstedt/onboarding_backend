1be92b055447e9f962298584ed6b6923e63511c8
Ricardo Muhlstedt <ricardomuhlstedt@hotmail.com>
2023-12-18 17:14:15 -0300

    integrating databese

--- src/entity/user-entity.ts
+++ src/entity/user-entity.ts
@@ -1,18 +1,25 @@
-import { Entity, PrimaryGeneratedColumn, Column } from "typeorm"
+import { Entity, PrimaryGeneratedColumn, Column } from "typeorm";
 
 @Entity()
 export class UserEntity {
+  @PrimaryGeneratedColumn()
+  id: number;
 
-    @PrimaryGeneratedColumn()
-    id: number
+  @Column()
+  firstName: string;
 
-    @Column()
-    firstName: string
+  @Column()
+  lastName: string;
 
-    @Column()
-    lastName: string
+  @Column()
+  age: number;
 
-    @Column()
-    age: number
+  @Column()
+  birthDate: string;
 
+  @Column()
+  email: string;
+
+  @Column()
+  password: string;
 }
